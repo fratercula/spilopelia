8adfa27ca79b6ac0cc9fc93294940d84f9fa6a5a
LoeiFy <LoeiFy@gmail.com>
2019-07-06 21:31:21 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spilopelia
+
 React Sub Router
---
+++ falco.config.js
@@ -0,0 +1,35 @@
+const { NODE_ENV } = process.env
+
+const output = {}
+
+if (NODE_ENV === 'docs') {
+  output.filename = 'index.[hash:8].js'
+}
+
+if (NODE_ENV === 'umd') {
+  output.library = 'spilopelia'
+  output.libraryTarget = 'umd'
+}
+
+module.exports = {
+  externals: [
+    {
+      name: 'react',
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      urls: 'https://cdn.staticfile.org/react/16.8.2/umd/react.production.min.js',
+    },
+    {
+      name: 'react-dom',
+      root: 'ReactDOM',
+      urls: 'https://cdn.staticfile.org/react-dom/16.8.2/umd/react-dom.production.min.js',
+    },
+  ],
+  npm: {
+    registry: 'https://registry.npm.taobao.org',
+  },
+  mode: NODE_ENV === 'development' ? NODE_ENV : 'production',
+  output,
+}
